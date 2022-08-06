205f6b627a8fa1df690ed729092e5c0a066c92f2
cxhuy <alvinkang1@gmail.com>
2022-08-06 23:14:35 +0900

    add router

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,6 +1,6 @@
 <nav class="bg-transparent border-gray-200 px-2 sm:px-4 py-2.5">
 	<div class="container flex flex-wrap justify-between items-center mx-auto">
-		<a href="/" class="flex items-center pl-3 sm:pl-0">
+		<a href="#/" class="flex items-center pl-3 sm:pl-0">
 			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
 				>웹소지지</span
 			>
@@ -30,35 +30,35 @@
 			<ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
 				<li>
 					<a
-						href="/platforms/munpia"
+						href="#/platforms/munpia"
 						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 						>문피아</a
 					>
 				</li>
 				<li>
 					<a
-						href="/platforms/novelpia"
+						href="#/platforms/novelpia"
 						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 						>노벨피아</a
 					>
 				</li>
 				<li>
 					<a
-						href="/platforms/kakaopage"
+						href="#/platforms/kakaopage"
 						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 						>카카오페이지</a
 					>
 				</li>
                 <li>
 					<a
-						href="/platforms/kakaostage"
+						href="#/platforms/kakaostage"
 						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 						>카카오스테이지</a
 					>
 				</li>
                 <li>
 					<a
-						href="/platforms/navernovel"
+						href="#/platforms/navernovel"
 						class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 						>네이버 웹소설</a
 					>
