581d10075e91cfaab207ea02d82b9288aa4ced5c
cxhuy <alvinkang1@gmail.com>
2022-08-07 22:20:44 +0900

    add noticebar

--- src/routes/platforms/navernovel.svelte
+++ src/routes/platforms/navernovel.svelte
@@ -1,5 +1,5 @@
 <script>
-    // import Noticebar from "../../components/noticebar.svelte";
+    import Noticebar from "../../components/noticebar.svelte";
     import PlatformInfo from "../../components/platform/platformInfo.svelte";
     import Heatmap from "../../components/platform/heatmap.svelte"
     import GenreTable from "../../components/platform/genreTable.svelte";
@@ -8,7 +8,7 @@
     import NavernovelData from "../../../public/jsondata/navernovel.json"
 </script>
 
-<!-- <Noticebar noticeMsg="네이버 웹소설은 최신화 조회수만 제공되고 있습니다."/> -->
+<Noticebar noticeMsg="네이버 웹소설은 최신화 조회수만 제공되고 있습니다."/>
 <PlatformInfo platformInfoData={NavernovelData.platformInfoData}/>
 <Heatmap heatmapData={NavernovelData.heatmapData} platformGenres={NavernovelData.platformInfoData.platformGenres}/>
 <GenreTable genreData={NavernovelData.genreData}/>
